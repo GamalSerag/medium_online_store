63266d6b44a0b5c38414156c4072d2a2d8954fb5
Gamal-modeem <gamal.serag@modeem.org.sa>
2026-02-19 14:49:23 +0200

    remove sqlite in production

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,13 +2,10 @@ from .base import *
 
 DEBUG = False
 
-DJANGO_SETTINGS_MODULE = 'config.settings.production'
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['al-serag.store', 'www.al-serag.store', 'dev.al-serag.store'])
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['al-serag.net', 'www.al-serag.net'])
-
-# Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
+# REQUIRED in production
+DATABASE_URL = env('DATABASE_URL')  # will raise error if missing
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres://alserag_user:STRONG_PASSWORD@127.0.0.1:5432/alserag_prod')
+    'default': env.db('DATABASE_URL'),
 }
-
-# test auto deploy2
